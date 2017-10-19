1081dd54b3fdca629966869c2ced4fda913c29e6
eduardoeq <32928916+eduardoeq@users.noreply.github.com>
2017-10-19 11:20:12 -0200

    a

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
-public class Cliente extends Main {
+public class Cliente {
+
+	String nome;
+	String cpf;
+	ArrayList<String> fone = new ArrayList<String>();
+	ArrayList<Integer> melhorDia = new ArrayList<Integer>();
+
+	public Cliente(String n, String f, int m) {
+		this.nome = n;
+		this.fone.add(f);
+		this.melhorDia.add(m);
+	}
+	
+	public void addFone(String f) {
+		this.fone.add(f);
+	}
+
+	public void addMelhorDia(int m) {
+		this.melhorDia.add(m);
+	}
 
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Cliente Thiago = new Cliente("Thiago Reinehr", "99564-5412", 10);
+		
+		Cliente Paulo = new Cliente("Paulo Almeida", "99564-1475", 15);
+		Paulo.addMelhorDia(30);
+		Paulo.addFone("98544-5254");
+		
+		System.out.println(Thiago.fone);
+		
+		System.out.println(Paulo.fone);
 	}
 	
 }
